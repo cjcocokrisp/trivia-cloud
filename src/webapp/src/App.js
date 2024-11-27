0f0b114ab3e0dd84b443c1cc0a12fb76633274e3
Lshaban313 <138370820+Lshaban313@users.noreply.github.com>
2024-11-26 19:50:17 -0500

    Update App.js
    just playing around with it just to see if im doing it correctly so far

--- src/webapp/src/App.js
+++ src/webapp/src/App.js
@@ -1,39 +1,110 @@
-import Game from './Game'
+import Game from './Game';
 import logo from './logo.svg';
 import './App.css';
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 import { useState } from 'react';
 
 function App() {
-    const [ username, setUsername ] = useState("Player");
-    const [ id, setId] = useState("");
-    const [ connectiontype, setConnectionType ] = useState("");
-    const [ inGame, setInGame ] = useState(false);
+    const [username, setUsername] = useState("Player");
+    const [id, setId] = useState("");
+    const [connectiontype, setConnectionType] = useState("");
+    const [inGame, setInGame] = useState(false);
+
+    // New state for dialog box
+    const [openDialog, setOpenDialog] = useState(false);
+    const [numQuestions, setNumQuestions] = useState(10);
+    const [category, setCategory] = useState("History");
 
     const validateCode = () => {
         setInGame(true);
         setConnectionType("join");
-    }
+    };
 
     if (!inGame)
-    return (
-        <div className="App">
-        <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <TextField id="outlined-basic" value={username} onChange={(event) => setUsername(event.target.value)} label="Username" variant='outlined'/>
-            <TextField id="outlined-basic" onChange={(event) => setId(event.target.value)} label="Game ID" variant='outlined'/>
-            <div className="button-container">
-                <Button onClick={() => {setInGame(true); setConnectionType("create")}} variant="contained">Create</Button>
-                <Button onClick={validateCode} variant="contained">Join</Button>
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <TextField
+                        id="outlined-basic"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                        label="Username"
+                        variant="outlined"
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        onChange={(event) => setId(event.target.value)}
+                        label="Game ID"
+                        variant="outlined"
+                    />
+                    <div className="button-container">
+                        {/* Create Button Opens Dialog */}
+                        <Button onClick={() => setOpenDialog(true)} variant="contained">
+                            Create
+                        </Button>
+                        <Button onClick={validateCode} variant="contained">
+                            Join
+                        </Button>
+                    </div>
+                </div>
+
+                {/* Dialog Box */}
+                <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+                    <DialogTitle>Game Settings</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            id="number-of-questions"
+                            label="Number of Questions"
+                            type="number"
+                            value={numQuestions}
+                            onChange={(e) => setNumQuestions(e.target.value)}
+                            fullWidth
+                            variant="outlined"
+                            margin="dense"
+                        />
+                        <TextField
+                            id="category"
+                            label="Category"
+                            value={category}
+                            onChange={(e) => setCategory(e.target.value)}
+                            fullWidth
+                            variant="outlined"
+                            margin="dense"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => setOpenDialog(false)}>Cancel</Button>
+                        <Button
+                            onClick={() => {
+                                setOpenDialog(false);
+                                setInGame(true); // Transition to Game
+                                setConnectionType("create"); // Maintain "create" connection type
+                            }}
+                            variant="contained"
+                        >
+                            Save
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
-        </div>
-        </div>
-    );
-    else 
-    return (
-        <Game connectiontype={connectiontype} username={username} id={id}/>
-    );
+        );
+    else
+        return (
+            <Game
+                connectiontype={connectiontype}
+                username={username}
+                id={id}
+                numQuestions={numQuestions} // Pass numQuestions to Game
+                category={category} // Pass category to Game
+            />
+        );
 }
 
 export default App;
+
