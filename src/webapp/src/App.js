7f7fb30cc024c735987df0a32c4d99e889b95b70
Lshaban313 <138370820+Lshaban313@users.noreply.github.com>
2024-11-26 20:31:13 -0500

    Update App.js

--- src/webapp/src/App.js
+++ src/webapp/src/App.js
@@ -1,186 +1,200 @@
-import Game from './Game'
+import Game from './Game';
 import logo from './logo.svg';
 import './App.css';
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 import FormControl from '@mui/material/FormControl';
 import Box from '@mui/material/Box';
-import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
 import { useState, useEffect } from 'react';
 
-function SimpleDialog({open, onClose, numQuestions, setNumQuestions, category, setCategory, showNumWarning, showCategoryWarning, createGame}) {
-    const [categories, setCategories] = useState([])
-    const handleClose = () => {
-        console.log();
-    }
+function SimpleDialog({
+    open,
+    onClose,
+    numQuestions,
+    setNumQuestions,
+    category,
+    setCategory,
+    showNumWarning,
+    showCategoryWarning,
+    createGame,
+}) {
+    const [categories, setCategories] = useState([]);
 
     const handleNumQuestionsChange = (event) => {
-        const inputValue = event.target.value
-        if (/^\d*$/.test(inputValue) ) {
-            console.log("valid input");
+        const inputValue = event.target.value;
+        if (/^\d*$/.test(inputValue)) {
             setNumQuestions(inputValue);
-        } else {
-            console.log('invalid input "${inputValue}"')
         }
-    }
+    };
 
     useEffect(() => {
-        fetch('https://opentdb.com/api_category.php').then(res => {
-            res.json().then(data => {
-                setCategories(data['trivia_categories']);
-                console.log(categories);
+        // Fetch trivia categories from the API
+        fetch('https://opentdb.com/api_category.php')
+            .then((res) => res.json())
+            .then((data) => {
+                setCategories(data.trivia_categories || []);
             });
-        });
     }, []);
 
     const handleCategoryChange = (event) => {
-        const selectedCategory = categories[event.target.value]
-        setCategory(selectedCategory)
-    }
+        setCategory(event.target.value);
+    };
 
     return (
-        <div className='simple-dialog'>
-            <Dialog open={open} onClose={onClose}>
-                <DialogTitle className="dialog-title">Game Settings</DialogTitle>
-                <TextField 
-                id="outlined-basic" 
-                value={numQuestions} 
-                onChange={handleNumQuestionsChange}
-                slotProps={{input: {
-                    inputMode: 'numeric',
-                    pattern: '[0-9]*', 
+        <Dialog
+            open={open}
+            onClose={onClose}
+            PaperProps={{
+                style: {
+                    padding: '20px',
+                    backgroundColor: '#f9f9f9',
+                    borderRadius: '10px',
                 },
             }}
-                label="Number of Questions" variant='outlined'/>
-            {showNumWarning && (
-                <div style={{ color: 'red', marginTop: '10px' }}>
-                    <strong>Warning:</strong> Please enter a value between 1-50
-                </div>
-            )}
-                <Box sx={{ minWidth: 120 }}>
-                    <FormControl fullWidth>
-                        <InputLabel id="select-label">Category</InputLabel>
-                            <Select
-                                labelId="select-label"
-                                id="simple-select"
-                                value={category['name']}
-                                label="Category"
-                                onChange={handleCategoryChange}
-                            >
-                                {
-                                    categories.map((value, index) => {
-                                        return (<MenuItem value={index}>{value['name']}</MenuItem>)
-                                    })
-                                }
-                                {/* <MenuItem value={9}>General Knowledge</MenuItem>
-                                <MenuItem value={10}>Entertainment: Books</MenuItem>
-                                <MenuItem value={11}>Entertainment: Film</MenuItem>
-                                <MenuItem value={12}>Entertainment: Music</MenuItem>
-                                <MenuItem value={13}>Entertainment: Musicals & Theatres</MenuItem>
-                                <MenuItem value={14}>Entertainment: Television</MenuItem>
-                                <MenuItem value={15}>Entertainment: Video Games</MenuItem>
-                                <MenuItem value={16}>Entertainment: Board Games</MenuItem>
-                                <MenuItem value={17}>Science & Nature</MenuItem>
-                                <MenuItem value={18}>Science: Computers</MenuItem>
-                                <MenuItem value={19}>Science: Mathematics</MenuItem>
-                                <MenuItem value={20}>Mythology</MenuItem>
-                                <MenuItem value={21}>Sports</MenuItem>
-                                <MenuItem value={22}>Geography</MenuItem>
-                                <MenuItem value={23}>History</MenuItem>
-                                <MenuItem value={24}>Politics</MenuItem>
-                                <MenuItem value={25}>Art</MenuItem>
-                                <MenuItem value={26}>Celebrities</MenuItem>
-                                <MenuItem value={27}>Animals</MenuItem>
-                                <MenuItem value={28}>Vehicles</MenuItem>
-                                <MenuItem value={29}>Entertainment: Comics</MenuItem>
-                                <MenuItem value={30}>Science: Gadgets</MenuItem>
-                                <MenuItem value={31}>Entertainment: Japanese Anime & Manga</MenuItem>
-                                <MenuItem value={32}>Entertainment: Cartoon & Animations</MenuItem> */}
-          
-                            </Select>
-                    </FormControl>
-                </Box>
-                {showCategoryWarning && (
-                    <div style={{ color: 'red', marginTop: '10px' }}>
-                        <strong>Warning:</strong> Please choose a category
+        >
+            <DialogTitle style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>
+                Game Settings
+            </DialogTitle>
+            <Box padding={2} display="flex" flexDirection="column" gap="20px">
+                <TextField
+                    id="outlined-basic"
+                    value={numQuestions}
+                    onChange={handleNumQuestionsChange}
+                    label="Number of Questions"
+                    variant="outlined"
+                    fullWidth
+                    inputProps={{
+                        inputMode: 'numeric',
+                        pattern: '[0-9]*',
+                    }}
+                />
+                {showNumWarning && (
+                    <div style={{ color: 'red', marginTop: '5px', fontSize: '0.9rem' }}>
+                        <strong>Warning:</strong> Please enter a value between 1 and 50.
                     </div>
                 )}
-                <Button onClick={createGame} variant="contained">Create</Button>
-            </Dialog>
-        </div>
-        
-    )
+                <FormControl fullWidth>
+                    <InputLabel id="select-label">Category</InputLabel>
+                    <Select
+                        labelId="select-label"
+                        id="simple-select"
+                        value={category}
+                        onChange={handleCategoryChange}
+                    >
+                        {categories.map((cat) => (
+                            <MenuItem key={cat.id} value={cat.id}>
+                                {cat.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                {showCategoryWarning && (
+                    <div style={{ color: 'red', marginTop: '5px', fontSize: '0.9rem' }}>
+                        <strong>Warning:</strong> Please choose a valid category.
+                    </div>
+                )}
+                <Button onClick={createGame} variant="contained" fullWidth style={{ marginTop: '10px' }}>
+                    Create
+                </Button>
+            </Box>
+        </Dialog>
+    );
 }
 
 function App() {
-    const [ username, setUsername ] = useState("Player");
-    const [ id, setId] = useState("");
-    const [ numQuestions, setNumQuestions] = useState(10);
-    const [ category, setCategory] = useState(0);
-    const [ connectiontype, setConnectionType ] = useState("");
-    const [ inGame, setInGame ] = useState(false);
+    const [username, setUsername] = useState('Player');
+    const [id, setId] = useState('');
+    const [numQuestions, setNumQuestions] = useState(10);
+    const [category, setCategory] = useState('');
+    const [connectiontype, setConnectionType] = useState('');
+    const [inGame, setInGame] = useState(false);
     const [isDialogOpen, setIsDialogOpen] = useState(false);
     const [showNumWarning, setShowNumWarning] = useState(false);
     const [showCategoryWarning, setShowCategoryWarning] = useState(false);
 
     const validateCode = () => {
         setInGame(true);
-        setConnectionType("join");
-    }
+        setConnectionType('join');
+    };
+
     const openDialog = () => {
         setIsDialogOpen(true);
-    }
+    };
+
     const createGame = () => {
-        if (numQuestions === "" || numQuestions < 1 || numQuestions > 50) {
+        if (numQuestions === '' || numQuestions < 1 || numQuestions > 50) {
             setShowNumWarning(true); // Show warning if the number is invalid
-        } else if (category === undefined || category < 9  || category > 32){
-            setShowCategoryWarning(true);  // Show warning if category isnt chosen
+        } else if (!category) {
+            setShowCategoryWarning(true); // Show warning if category isn't chosen
         } else {
-            //console.log(`num questions: "${numQuestions}" category id: "${category}"`);
             setInGame(true); // Create the game
-            setConnectionType("create");
+            setConnectionType('create');
             setShowNumWarning(false);
             setShowCategoryWarning(false);
         }
-    }
+    };
+
     const closeDialog = () => {
         setIsDialogOpen(false);
-    }
-    
-    if (!inGame)
-    return (
-        <div className="App">
-        <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <TextField id="outlined-basic" value={username} onChange={(event) => setUsername(event.target.value)} label="Username" variant='outlined'/>
-            <TextField id="outlined-basic" onChange={(event) => setId(event.target.value)} label="Game ID" variant='outlined'/>
-            <div className="button-container">
-                <Button onClick={openDialog} variant="contained">Create</Button>
-                <Button onClick={validateCode} variant="contained">Join</Button>
-            </div>
-        </div>
-            <SimpleDialog 
-            open={isDialogOpen} 
-            onClose={closeDialog} 
-            numQuestions={numQuestions} 
-            setNumQuestions={setNumQuestions} 
-            category={category} 
-            setCategory={setCategory} 
-            showNumWarning={showNumWarning}
-            showCategoryWarning={showCategoryWarning}
-            createGame={createGame} />
-        </div>
-    );
-    else
-    return (
-        <Game connectiontype={connectiontype} username={username} numQuestions={numQuestions} category={category} id={id}/>
-    );
+    };
 
+    if (!inGame)
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <TextField
+                        id="outlined-basic"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                        label="Username"
+                        variant="outlined"
+                        style={{ marginBottom: '20px' }}
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        onChange={(event) => setId(event.target.value)}
+                        label="Game ID"
+                        variant="outlined"
+                        style={{ marginBottom: '20px' }}
+                    />
+                    <div className="button-container" style={{ display: 'flex', gap: '10px' }}>
+                        <Button onClick={openDialog} variant="contained">
+                            Create
+                        </Button>
+                        <Button onClick={validateCode} variant="contained">
+                            Join
+                        </Button>
+                    </div>
+                </div>
+                <SimpleDialog
+                    open={isDialogOpen}
+                    onClose={closeDialog}
+                    numQuestions={numQuestions}
+                    setNumQuestions={setNumQuestions}
+                    category={category}
+                    setCategory={setCategory}
+                    showNumWarning={showNumWarning}
+                    showCategoryWarning={showCategoryWarning}
+                    createGame={createGame}
+                />
+            </div>
+        );
+    else
+        return (
+            <Game
+                connectiontype={connectiontype}
+                username={username}
+                numQuestions={numQuestions}
+                category={category}
+                id={id}
+            />
+        );
 }
 
 export default App;
